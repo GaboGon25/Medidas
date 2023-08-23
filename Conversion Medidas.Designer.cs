4b1d99bfc826f949d2db4c43fcba08b98d487623
gabri <gabri@DESKTOP-G23QIT1>
2023-08-22 21:23:48 -0600

    Agregar archivos de proyecto.

---
+++ Conversion Medidas.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Medidas
+{
+    partial class Conversion_Medidas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Conversion_Medidas));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMetros = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCentimetros = new System.Windows.Forms.TextBox();
+            this.txtPulgadas = new System.Windows.Forms.TextBox();
+            this.txtPies = new System.Windows.Forms.TextBox();
+            this.txtYardas = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MEDIDAS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(91, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 69);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "INTRODUZCA LOS METROS (M) :";
+            // 
+            // txtMetros
+            // 
+            this.txtMetros.Location = new System.Drawing.Point(42, 143);
+            this.txtMetros.Name = "txtMetros";
+            this.txtMetros.Size = new System.Drawing.Size(307, 20);
+            this.txtMetros.TabIndex = 3;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(45, 186);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(119, 63);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "CONVERTIR";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(231, 186);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(119, 63);
+            this.btnNuevo.TabIndex = 5;
+            this.btnNuevo.Text = "NUEVO";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CENTIMETROS (Cm) :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PULGADAS (In) :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 365);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "PIES (Ft) :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(228, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "YARDAS (Yd) :";
+            // 
+            // txtCentimetros
+            // 
+            this.txtCentimetros.Location = new System.Drawing.Point(45, 303);
+            this.txtCentimetros.Name = "txtCentimetros";
+            this.txtCentimetros.Size = new System.Drawing.Size(116, 20);
+            this.txtCentimetros.TabIndex = 10;
+            // 
+            // txtPulgadas
+            // 
+            this.txtPulgadas.Location = new System.Drawing.Point(234, 303);
+            this.txtPulgadas.Name = "txtPulgadas";
+            this.txtPulgadas.Size = new System.Drawing.Size(116, 20);
+            this.txtPulgadas.TabIndex = 11;
+            // 
+            // txtPies
+            // 
+            this.txtPies.Location = new System.Drawing.Point(45, 392);
+            this.txtPies.Name = "txtPies";
+            this.txtPies.Size = new System.Drawing.Size(116, 20);
+            this.txtPies.TabIndex = 12;
+            // 
+            // txtYardas
+            // 
+            this.txtYardas.Location = new System.Drawing.Point(234, 392);
+            this.txtYardas.Name = "txtYardas";
+            this.txtYardas.Size = new System.Drawing.Size(116, 20);
+            this.txtYardas.TabIndex = 13;
+            // 
+            // Conversion_Medidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.txtYardas);
+            this.Controls.Add(this.txtPies);
+            this.Controls.Add(this.txtPulgadas);
+            this.Controls.Add(this.txtCentimetros);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtMetros);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Conversion_Medidas";
+            this.Text = "Conversion_Medidas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMetros;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCentimetros;
+        private System.Windows.Forms.TextBox txtPulgadas;
+        private System.Windows.Forms.TextBox txtPies;
+        private System.Windows.Forms.TextBox txtYardas;
+    }
+}
---
+++ Conversion Medidas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medidas
+{
+    public partial class Conversion_Medidas : Form
+    {
+        public Conversion_Medidas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            double mtr, ctm, yd, pul, ft;
+
+            mtr = double.Parse(txtMetros.Text);
+
+            ctm = (mtr * 100);
+
+            txtCentimetros.Text = ctm.ToString();
+
+            pul = (ctm / 2.54);
+
+            txtPulgadas.Text = pul.ToString();
+
+            ft = (pul / 12);
+
+            txtPies.Text = ft.ToString();
+
+            yd = (ft / 3);
+
+            txtYardas.Text = yd.ToString();
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtMetros.Clear();
+            txtCentimetros.Clear();
+            txtPulgadas.Clear();
+            txtPies.Clear();
+            txtYardas.Clear();
+        }
+    }
+}
